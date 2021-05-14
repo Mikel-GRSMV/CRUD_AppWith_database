dc41ea1d42a272a9a9088e98eef7eec820f520ac
Mikel-GRSMV <mikhail.gerasimov.2000@mail.ru>
2021-05-14 19:18:46 +0300

    CRUD_AppWith_database
    Implementing a CRUD application using a database PostgresSQL.

---
+++ src/main/java/ru/gerasimov/springmvc/config/SpringMVCDispetcherServletInitializer.java
@@ -0,0 +1,35 @@
+package ru.gerasimov.springmvc.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class SpringMVCDispetcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
---
+++ src/main/java/ru/gerasimov/springmvc/dao/PersonMapper.java
@@ -0,0 +1,23 @@
+package ru.gerasimov.springmvc.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.gerasimov.springmvc.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Person person = new Person();
+
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+}
+
